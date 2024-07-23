53eef57509f102fd1ed44b720eb3f241fdad2e78
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-23 13:13:58 +0530

    Update script.js

--- day10/form-event/task7/script.js
+++ day10/form-event/task7/script.js
@@ -1,1 +1,8 @@
+let data = document.querySelector('#form');
+data.addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission
+    let changedata = new FormData(e.target);
+    const finaldata = Object.fromEntries(changedata.entries());
+    console.log(finaldata);
+});
 
