5bcd998b797738414490b670dbd7fe745561549d
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-23 16:55:59 +0530

    Update script.js

--- day10/form-event/task8/script.js
+++ day10/form-event/task8/script.js
@@ -1,1 +1,6 @@
-
+const item=document.querySelector('#items')
+const displaypara=document.querySelector('#para')
+item.addEventListener('change', function(e){
+    // const value=e.target.value
+    displaypara.textContent=`you have Selected value ${e.target.value}`
+})
