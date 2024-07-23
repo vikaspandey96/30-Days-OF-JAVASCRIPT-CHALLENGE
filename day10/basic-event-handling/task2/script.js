83fe33ed1aefcc7765c6dfda46142988bc7c0367
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-23 11:17:00 +0530

    Update script.js

--- day10/basic-event-handling/task2/script.js
+++ day10/basic-event-handling/task2/script.js
@@ -1,1 +1,9 @@
+const img=document.querySelector('img')
+img.addEventListener('dblclick',function(){
+    if(img.style.display==="none"){
+        img.style.display='block'
+    }else{
+        img.style.display='none'
+    }
+})
 
