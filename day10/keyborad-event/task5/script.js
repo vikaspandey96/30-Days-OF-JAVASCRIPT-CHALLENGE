50db30e0dce0b98ac01441942ac8c4a1be454dda
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-23 11:59:39 +0530

    Update script.js

--- day10/keyborad-event/task5/script.js
+++ day10/keyborad-event/task5/script.js
@@ -2,3 +2,4 @@ const fullname=document.querySelector('#text-input')
 fullname.addEventListener('keydown', function(e){
     console.log(`Key Pressed: ${e.key}`);
 })
+//output--Key Pressed: ArrowDown
