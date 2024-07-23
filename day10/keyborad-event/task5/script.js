27aa7cb589d7b2edd833039220536349a522b74b
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-23 12:18:59 +0530

    Update script.js

--- day10/keyborad-event/task5/script.js
+++ day10/keyborad-event/task5/script.js
@@ -3,3 +3,17 @@ fullname.addEventListener('keydown', function(e){
     console.log(`Key Pressed: ${e.key}`);
 })
 //output--Key Pressed: ArrowDown
+
+
+
+
+//OR
+//It use when you want only keydown console
+const fullname=document.querySelector('#text-input')
+fullname.addEventListener('keydown', function(e){
+    if(e.key==='ArrowDown'){
+        console.log(e.key);
+    }
+   
+})
+
