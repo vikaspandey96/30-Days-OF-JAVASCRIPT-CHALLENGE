36e379e50bf3785e719e455bd33070ec6087938c
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 20:43:42 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity3-array-methods/task8/script.js
@@ -0,0 +1,6 @@
+// Task 8: Use the filter method to create a new array with only even numbers and log the new array. 
+let numbers=[2,4,7,13,25,16,12,22,45]
+let evenNumber=numbers.filter((num) => num%2===0)
+console.log(evenNumber);
+
+// output :[2, 4, 16, 12, 22]
