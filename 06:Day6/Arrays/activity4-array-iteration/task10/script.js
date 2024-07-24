6b92644482a58051115df2c2f92e78a93e5bcb25
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 20:52:09 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity4-array-iteration/task10/script.js
@@ -0,0 +1,11 @@
+// Task 10: Use a for loop to iterate over the array and log each elements to the console.
+let clothes=["shirts","pants","suit","jeans"]
+for(let i=0; i<clothes.length;i++){
+    console.log(clothes[i]);
+}
+
+/* output:
+shirts
+pants
+suit
+jeans */
