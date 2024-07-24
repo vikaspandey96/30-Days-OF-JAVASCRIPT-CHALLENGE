31f2731b3d2abec38c8ae7ef15f85e91ecbdc4b8
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 12:57:24 +0530

    Create script.js

---
+++ 04:Day4/activity1-for-loop/task1/script.js
@@ -0,0 +1,14 @@
+// Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+for(let i=1;i<=10; i++){
+    console.log(i);
+}
+// output: 1
+//         2
+//         3
+//         4
+//         5
+//         6
+//         7
+//         8
+//         9
+//         10
