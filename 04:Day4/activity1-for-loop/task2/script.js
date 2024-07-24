675871283b2acef07e3975fceae05cb2ce4c0563
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 13:03:05 +0530

    Create script.js

---
+++ 04:Day4/activity1-for-loop/task2/script.js
@@ -0,0 +1,19 @@
+// Task 2: Write a program to print the multiplication table of 5 using a for loop.
+let num=5;
+for(i=0; i<=10;i++){
+    console.log(`${num}*${i} = ${num*i}`);
+}
+
+//This output on browser console
+// output :
+// 5*0 0
+//  5*1 = 5
+//  5*2 =10
+//  5*3 =15
+//  5*4 =20
+//  5*5 =25
+//  5*6 =30
+//  5*7 =35
+//  5*8 =40
+//  5*9 =45
+//  5*10= 50
