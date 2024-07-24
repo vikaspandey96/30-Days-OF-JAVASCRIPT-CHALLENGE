10597384e9a2094871b050253cd5ccde560d1912
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 13:08:01 +0530

    Create script.js

---
+++ 04:Day4/activity2-while-loop/task3/script.js
@@ -0,0 +1,9 @@
+// Task3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+let i=1
+let sum=0
+while(i<=10){
+    sum+=i
+    i++
+}console.log(`sum of 1 to 10 number is: ${sum}`);
+
+// output: sum of 1 to 10 number is:55
