363601e5efd125426f020a12fab4402820fec484
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 10:42:07 +0530

    Create script.js

---
+++ 01:Day1/Activity 3: Data types/task4/script.js
@@ -0,0 +1,13 @@
+let str='hello';
+let num=44;
+let boolval=false;
+let obj={
+    fname:"Vikas",
+    lname:'pandey'
+}
+let arr=[3,4,5,"apple",'cat']
+console.log(typeof str);  //output string
+console.log(typeof num); // output number
+console.log(typeof boolval);// boolean
+console.log(typeof obj); //output object
+console.log(typeof arr); //output object
