aa97ced1b67791546907c330e8e892f5012c2ea2
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 12:02:32 +0530

    Create script.js

---
+++ 03: Day3/activity1-if-else-statements/task1/script.js
@@ -0,0 +1,12 @@
+// task 1:Write a program to check if a number is positive, negative, or zero and log the result to the console
+let num=0 
+if(num>0){
+    console.log(`This number ${num} is positive`);
+}else if(num<0){
+    console.log(`this number ${num} is negative`);
+}else{
+    console.log(`This number ${num} is zero`);
+}
+
+
+// output : This number 0 is zero
