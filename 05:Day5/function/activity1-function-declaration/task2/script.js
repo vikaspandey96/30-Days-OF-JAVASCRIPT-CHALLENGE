98697275edf861215ae1e27349559aa0d71c3de0
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 15:21:17 +0530

    Create script.js

---
+++ 05:Day5/function/activity1-function-declaration/task2/script.js
@@ -0,0 +1,6 @@
+// Task 2: Write a function to calculate the square of number and return the result. 
+function squareNumber(number){
+    return number*number
+}
+let number=4
+console.log(squareNumber(number)) //output :16
