9380c10608b69b021459366068f74ce1e95988d0
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 12:39:53 +0530

    Create script.js

---
+++ 03: Day3/activity4-conditional-ternary-operator/task6/script.js
@@ -0,0 +1,6 @@
+// Task 6: Write a program that uses the ternary opeator to check if a number is even or odd and log the result to the console.
+let num =35 
+let isEvenOdd=num%2===0 ? `This number ${num} is even nubmer` : `This number ${num} is odd number`
+console.log(isEvenOdd);
+
+// output: This number 35 is odd number
