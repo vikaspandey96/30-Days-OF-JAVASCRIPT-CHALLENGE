0fbfc6595070622c8fbeb84a36fc252cdccb9e5b
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:47:06 +0530

    Create script.js

---
+++ 02:Day2/Activity-4-Logical-Operators/task13/script.js
@@ -0,0 +1,6 @@
+// task 13: Write a program that uses the ! operator to combines two conditon and log the result to the console
+let gender="male";
+if(gender!=='female'){
+    console.log(gender);
+}
+// output: male
