ba03689c27489128b038c3fe63d96824882aeda6
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 15:30:17 +0530

    Create script.js

---
+++ 05:Day5/function/activity2-function-expression/task4/script.js
@@ -0,0 +1,8 @@
+// Task 4: Write a function expression to concatenate two strings and return the result.
+const concatString=function value(str1,str2){
+    return str1.concat(str2)
+}
+let str=concatString('vikas',' pandey')
+console.log(str);
+
+//output: vikas pandey
