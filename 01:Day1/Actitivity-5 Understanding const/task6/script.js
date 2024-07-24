8bbfbbcfbdf26823e9ca28a267974e4b5375e596
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 10:51:35 +0530

    Update script.js

--- 01:Day1/Actitivity-5 Understanding const/task6/script.js
+++ 01:Day1/Actitivity-5 Understanding const/task6/script.js
@@ -1,4 +1,4 @@
-task6: try reassigning a varible declared with const and observe the error.
+//task6: try reassigning a varible declared with const and observe the error.
 const val=44
 val='vvv'
 console.log(val);
