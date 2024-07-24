dd781897e72640b3dcb5daab3c162d35ec816d8d
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 15:36:24 +0530

    Create script.js

---
+++ 05:Day5/function/activity3-arrow-function/task6/script.js
@@ -0,0 +1,5 @@
+// Task 6: Write an arrow function to check if a string contains a specific character and return a boolean value. 
+const checkChar=(str,char)=> str.includes(char)
+console.log(checkChar("vikas",'a'));
+
+// output: true 
