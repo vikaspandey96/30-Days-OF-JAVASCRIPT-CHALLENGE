e02a9eb060f85110ed22f22e333393b9b38de34b
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 16:16:24 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity2-Array-methods-basic/task4/script.js
@@ -0,0 +1,12 @@
+// Task4: Use the pop method to remove the last element from the array and log the updated array.
+let color=['red','green','blue','yellow']
+color.pop()
+console.log(color);
+
+// output:
+// (3) ['red', 'green', 'blue']
+// 0:"red"
+// 1:"green"
+// 2: "blue"
+// length: 3
+// [[Prototype]]: Array(0)
