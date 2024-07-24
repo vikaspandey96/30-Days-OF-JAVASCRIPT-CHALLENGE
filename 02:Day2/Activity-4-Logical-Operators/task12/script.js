3c2ad36259ffe73c783a8b07d299e6fcab55670c
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:44:25 +0530

    Create script.js

---
+++ 02:Day2/Activity-4-Logical-Operators/task12/script.js
@@ -0,0 +1,7 @@
+// task 12: Write a program that uses the || opeator to combine two conditons and log the result to the console
+let color='black';
+age=13;
+if(age<=18 || color==='black'){
+    console.log('you have black color pen');
+}
+//output : you have black color pen
