8b7c6a50d86cc4e90f2d74a3a294dde0a0417cc4
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 21:04:53 +0530

    Create script.js

---
+++ 07:Day7/object/activity7-obejct-creation-access/task2/script.js
@@ -0,0 +1,12 @@
+// Task 2: Acess and log the title and author properties of the book object. 
+const books={
+    title:'java',
+    author:'Rohit',
+    year:2000
+}
+console.log(books.title); // using dot method
+console.log(books["author"]); // using bracket [] method
+
+// output
+// java
+// Rohit
