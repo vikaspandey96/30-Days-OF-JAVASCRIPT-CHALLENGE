e8c9cbeed8859e66d4d2ba7e029ade953d3161c7
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:17:21 +0530

    Create script.js

---
+++ 02:Day2/Activity1-Arithmatic-operations/task5/script.js
@@ -0,0 +1,6 @@
+// task 5: Write a program to find the remainder when one number is divided by anoter and log the result to the console
+let firstNumber= 200;
+let secondNumber=10;
+let remainderNum=firstNumber%secondNumber
+console.log(remainderNum);
+// output: 0
