df3e707a437caf3b2b7ef3b1dbbd9c2161cb6864
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 13:19:08 +0530

    Create script.js

---
+++ 04:Day4/activity3-do-while-loop/task6/script.js
@@ -0,0 +1,12 @@
+// Task 6:Write a program to calculate the factorial of a numbers using do..while loop. 
+let num=5;
+let fact=1;
+let i=num;
+do{
+    fact=fact*i
+    i--
+}while(i>0)
+    console.log(`factorial of ${num} is ${fact}`);
+
+
+// output : factorai of 5 is 120    
