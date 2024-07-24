eea300cd2625d269b2565300bd462a1cde73fd9b
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:52:01 +0530

    Update script.js

--- 02:Day2/Activity-5-Ternary-Operator/task15/script.js
+++ 02:Day2/Activity-5-Ternary-Operator/task15/script.js
@@ -1,4 +1,5 @@
-// task 15: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+// task 15: Write a program that uses the ternary operator to check if a number is positive or negative and
+// log the result to the console.
 let  age=13 
 let isDrive=age>=18 ? console.log("you can drive") : console.log('you cannot drive');
 
