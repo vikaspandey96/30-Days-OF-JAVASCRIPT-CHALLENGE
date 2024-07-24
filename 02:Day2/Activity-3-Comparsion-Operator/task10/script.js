6778134f4c84109305bf1bc3bdca58b73c2f797e
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:37:37 +0530

    Create script.js

---
+++ 02:Day2/Activity-3-Comparsion-Operator/task10/script.js
@@ -0,0 +1,5 @@
+// task 10: Write a program to compare two numbers using == and === and log the result to the console
+let str='33'
+let num=33
+console.log(num==str);  //output true
+console.log(num===str); // output false
