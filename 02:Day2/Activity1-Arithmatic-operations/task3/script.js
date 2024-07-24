0c33b433cc4695d45497faf186cb0454b6170a78
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:11:01 +0530

    Create script.js

---
+++ 02:Day2/Activity1-Arithmatic-operations/task3/script.js
@@ -0,0 +1,6 @@
+// task 3: Write a program to multiply two numbers and log the result to the console 
+let number1=20;
+let number2=10;
+let multiply=number1*number2;
+console.log(multiply);
+// output :200
