d2950c5755666e6e2d96ab4eb386017b789695c1
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 20:47:40 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity3-array-methods/task9/script.js
@@ -0,0 +1,6 @@
+// Task 9: Use the reduce method to calculate the sum of all numbers in the array and log the result. 
+let value=[23,4,5,6,7]
+let sumoFValue=value.reduce((sum,val)=> sum+val, 0)
+console.log(sumoFValue);
+
+// output : 45
