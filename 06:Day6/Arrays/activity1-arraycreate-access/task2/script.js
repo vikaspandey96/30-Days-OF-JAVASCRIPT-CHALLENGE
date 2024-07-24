a24abdcff737e442080ab74ef904534025ad68bc
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 16:07:47 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity1-arraycreate-access/task2/script.js
@@ -0,0 +1,6 @@
+// Task 2: Access the first and last element of the array and log them to the console.
+let value=[4,5,6,7,8]
+let firstElement=value[0]
+let lastElement=value[value.length-1]
+console.log(firstElement);   //output 4
+console.log(lastElement);   // output 8
