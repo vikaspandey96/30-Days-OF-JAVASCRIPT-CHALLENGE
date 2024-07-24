343b50172fed7b70ac9c2439db8b168849b17133
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 16:23:49 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity2-Array-methods-basic/task6/script.js
@@ -0,0 +1,6 @@
+// Task 6:Use the unshift method to add new number to the beginning of the array and log the updated array. 
+let vegetables=['cabbage','onion','capsicum','cauliflower']
+vegetables.unshift('patato')
+console.log(vegetables);
+
+output: ['patato','cabbage', 'onion', 'capsicum', 'cauliflower']
