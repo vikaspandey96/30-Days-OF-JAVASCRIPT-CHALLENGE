5fd6a9dbf1973d38616d8a8fcc968c93929bd6a7
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 20:55:13 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity4-array-iteration/task11/script.js
@@ -0,0 +1,11 @@
+// Task 11: Use the ForEach method to iterate over the array and log each elements to the console.
+let values=[1,2,3,4,5,6]
+values.forEach((val)=> console.log(val))
+
+/* output:
+1
+2
+3
+4
+5
+6 */
