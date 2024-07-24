d14d8c0abeab8fa6dc93f37a9a87e45a5f895c49
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 16:20:06 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity2-Array-methods-basic/task5/script.js
@@ -0,0 +1,6 @@
+// Task 5: Use the shift method to remove the first element from the array and log the updated array.
+let itemList=['chair','table','mirror','pen']
+itemList.pop()
+console.log(itemList);
+
+// output: ['chair', 'table', 'mirror']
