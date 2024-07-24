f8f3e6e8a30fab51b9b8f2b477ed51d913766ed1
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 21:00:55 +0530

    Create script.js

---
+++ 07:Day7/object/activity7-obejct-creation-access/task1/script.js
@@ -0,0 +1,15 @@
+// Task 1: Create an object representing a book with properties like title,author, and year and log the obejct to the console. 
+const book={
+    title: 'fortune',
+    author:'Arun',
+    year:2014
+}
+console.log(book);
+
+// output:
+/* 
+{title: 'fortune', author: 'Arun', year: 2014}
+author: "Arun"
+title:"fortune"
+year:2014
+[[Prototype]]: Object */
