f37732ef4549eb17787d7899ae237ff556076e6f
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:35:01 +0530

    Update script.js

--- 02:Day2/Activity-3-Comparsion-Operator/task9/script.js
+++ 02:Day2/Activity-3-Comparsion-Operator/task9/script.js
@@ -3,5 +3,5 @@ let big=30
 let small=20
 let biggerNum=big>=small
 let smallerNum=big<=small
-console.log(biggerNum); //output true
-console.log(smallerNum); // output false
+console.log(biggerNum); //output: true
+console.log(smallerNum); // output: false
