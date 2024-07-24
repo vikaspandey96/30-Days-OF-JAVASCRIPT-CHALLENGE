e2c367a356b810de688b6424ce37239d2d21eaed
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 15:18:31 +0530

    Create script.js

---
+++ 05:Day5/function/activity1-function-declaration/task1/script.js
@@ -0,0 +1,11 @@
+// Task 1: Write a function to check if a number is even or odd and log the result to the console. 
+function isEvenOdd(number){
+    if(number%2===0){
+        console.log(`This number ${number} is even`);
+    }else{
+        console.log(`This number ${number} is odd`);
+    }
+}
+isEvenOdd(33)
+ 
+//output: This number 33 is odd
