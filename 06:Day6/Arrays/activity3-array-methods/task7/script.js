71fb7aaf965364dc127c97b6dd9bfb5140a6d3e9
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 20:40:07 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity3-array-methods/task7/script.js
@@ -0,0 +1,6 @@
+// Task 7: Use the map method to create a new array where each number is doubled and log the new array.
+let arr=[2,3,4,5,8,10]
+const result=arr.map((num) => num*2)
+console.log(result);
+
+// output: [4, 6, 8, 10, 16, 20]
