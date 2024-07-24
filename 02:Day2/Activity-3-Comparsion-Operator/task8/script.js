7a1173a6cdc9022067bc4fd1452c5f68009faf47
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:30:22 +0530

    Create script.js

---
+++ 02:Day2/Activity-3-Comparsion-Operator/task8/script.js
@@ -0,0 +1,7 @@
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console 
+let greater =33
+let smaller=22
+let greaterNum=greater>smaller;
+let smallerNum=greater<smaller
+console.log(greaterNum); //output true
+console.log(smallerNum); // output false
