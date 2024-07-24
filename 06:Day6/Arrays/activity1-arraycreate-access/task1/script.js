d9aa06fde8b9d43d50c240465edaa65d8e24f71d
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 16:04:22 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity1-arraycreate-access/task1/script.js
@@ -0,0 +1,13 @@
+// Task 1: Create an arary of numbers from 1 and 5 log the array to the console.
+let arr=[1,2,3,4,5]
+console.log(arr);
+
+// output:
+// (5) [1, 2, 3, 4, 5]
+// 0:1
+// 1:2
+// 2:3
+// 3:4
+// 4:5
+// length: 5
+// [[Prototype]] : Array(0)
