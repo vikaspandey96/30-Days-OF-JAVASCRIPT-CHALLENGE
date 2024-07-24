e95ace2fe53cc937710bc14dbec497fe1f35b637
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 12:06:24 +0530

    Create script.js

---
+++ 03: Day3/activity1-if-else-statements/task2/script.js
@@ -0,0 +1,9 @@
+// task 2: Write a program to check if a person is eligible to vote (age>=18) and log the result to the console.
+let age=19;
+if(age>=18){
+    console.log(`you are ${age} year old. you are eligible to vote`);
+}else{
+    console.log(`you are ${age} year old. you are not elgible to vote`);
+}
+
+// output : you are 19 year old. you are eligible to vote
