385c181f98e556323f3b7b585fd72829f037bde6
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 16:12:45 +0530

    Create script.js

---
+++ 06:Day6/Arrays/activity2-Array-methods-basic/task3/script.js
@@ -0,0 +1,14 @@
+// Task 3: Use the push method to add a new number to the end of the array and log the updated array. 
+let mixValue=[2,3,'vikas','hii']
+mixValue.push('Green')
+console.log(mixValue);
+
+/* output
+ (5) [2, 3, 'vikas', 'hii', 'Green']
+0:2
+1:3
+2:"vikas"
+3:"hii"
+4:"Green"
+length: 5
+[[Prototype]]: Array(0) */
