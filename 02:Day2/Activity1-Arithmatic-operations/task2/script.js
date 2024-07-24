6506b7b1e6c8e3be377bc3a6518fb1a6baf6f4ed
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:07:27 +0530

    Create script.js

---
+++ 02:Day2/Activity1-Arithmatic-operations/task2/script.js
@@ -0,0 +1,6 @@
+// Task 2: Write a program to subtract two number and log the result to the console 
+let numONe=20;
+let numTwo=10
+let finalSubtract=numONe-numTwo
+console.log(finalSubtract);
+// output : 10
