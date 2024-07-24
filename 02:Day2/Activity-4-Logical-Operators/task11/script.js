31b7200bdb668fdae190c4bcfc986404978398bb
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 11:41:28 +0530

    Create script.js

---
+++ 02:Day2/Activity-4-Logical-Operators/task11/script.js
@@ -0,0 +1,7 @@
+// task 11: Write a program that uses && operator to combines two conditons and log the result to the console
+let age=18;
+let car=true;
+if(age<=18 && car){
+    console.log("you can drive");
+}
+// ouptput : you can drive
