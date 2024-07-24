b6e31575aa4fdfd379807f577e715fe4bc8e72cb
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-24 13:11:52 +0530

    Create script.js

---
+++ 04:Day4/activity2-while-loop/task4/script.js
@@ -0,0 +1,18 @@
+// Task 4:Write a program to print numbers from 10 to 1 using while loop.
+let i=10;
+while(i>0){
+    console.log(i);
+    i--
+}
+
+// output: 
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
