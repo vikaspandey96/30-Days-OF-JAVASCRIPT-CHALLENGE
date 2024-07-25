5bc22486764b54be7e49fb3c6ce5a561669d92bc
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 10:20:12 +0530

    Create script.js

---
+++ 07:Day7/object/activity5-obejct-iteration/task9/script.js
@@ -0,0 +1,11 @@
+// Task 9: Use object.key and object.values methods to log all the keys and values of the book object. 
+const book={
+    title:'OS',
+    author:'RAm',
+    year:2020
+}
+console.log("all keys are:"   ,Object.keys(book));
+console.log("all values are:", Object.values(book));
+
+// output:all keys are: ['title', 'author', 'year']
+// all values are:['OS', 'RAm', 2020]
