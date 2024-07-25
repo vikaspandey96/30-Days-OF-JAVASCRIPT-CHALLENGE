8f91715e48754d1b630eca031b1c18f9defa80bc
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 10:45:17 +0530

    Create script.js

---
+++ 08:Day8/ES6-Features/activity3-spread-restoperator/task5/script.js
@@ -0,0 +1,6 @@
+// Task 5: Use the spread operator to create a new array that includes all elements of an existing array plus additional elements, and log the array to the console.
+let fruits=['banana','Apple','graps']
+let addMoreFruits=[...fruits,'Guava','strawberry']
+console.log(addMoreFruits);
+
+output:['banana', 'Apple', 'graps', 'Guava', 'strawberry']
