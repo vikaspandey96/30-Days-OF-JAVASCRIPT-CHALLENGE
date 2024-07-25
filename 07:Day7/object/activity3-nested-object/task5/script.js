8a589667a5adb0c0f4e859def7216d5168107ee8
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 10:03:32 +0530

    Create script.js

---
+++ 07:Day7/object/activity3-nested-object/task5/script.js
@@ -0,0 +1,20 @@
+/* Activity3: Nested Obejct
+Task 5:Create a nested object representing a library with properties like name and books (an array of book objects), and log the library object to the console. */
+libaray={
+    name:'Sham Libaray',
+    books:[
+        {
+            title:'java script',
+            author:'john',
+            year:1996
+        },
+        {
+            title:'C++',
+            author:'Suman',
+            year:2005
+        }
+    ]
+}
+console.log(libaray);
+
+// output:{name: 'Sham Libaray', books: Array(2)}
