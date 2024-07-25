7dbfa77b4012923bc87fcdd7fe81fa4d20424c6b
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 10:36:29 +0530

    Create script.js

---
+++ 08:Day8/ES6-Features/activity2-Destructuring/task3/script.js
@@ -0,0 +1,5 @@
+// Task 3: Use array Destructuring  to extract the first and second elements from an array of numbers and log them to the console. 
+let numbers=[1,2]
+let [firstNumber,secondNumber]=numbers
+console.log(firstNumber); //output :1
+console.log(secondNumber);// output 2
