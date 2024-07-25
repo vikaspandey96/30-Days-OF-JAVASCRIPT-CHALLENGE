9e27f744f5d5ead2759afd2ad962404a1413a9c9
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 10:25:13 +0530

    Create script.js

---
+++ 08:Day8/ES6-Features/activity1-templateLiterals/task1/script.js
@@ -0,0 +1,7 @@
+// Task 1: Use template literals to create a string that includes variable for a person' name, age and log the string to the console.
+let name="vikas"
+let age=33
+let personDetails=`Person name is ${name} and person age is ${age}` 
+console.log(personDetails);
+
+// output: Person name is vikas and person age is 33
