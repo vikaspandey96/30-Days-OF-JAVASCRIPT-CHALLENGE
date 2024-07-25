69235250ccc76f92276088e323b2ef4725d84fd4
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 10:28:51 +0530

    Create script.js

---
+++ 08:Day8/ES6-Features/activity1-templateLiterals/task2/script.js
@@ -0,0 +1,13 @@
+// Task 2:Create a multi-line string using template literals and log it to the console.
+let peom=`O ere my love you Lilac fair,
+   Wi purple blossoms to hte spring,
+     and I ,a brid to shlter there,
+        when wearied on my little wing!`
+
+console.log(peom);
+
+// outupt:
+// O ere my love you Lilac fair,
+//    Wi purple blossoms to hte spring,
+//      and I ,a brid to shlter there,
+//         when wearied on my little wing!
