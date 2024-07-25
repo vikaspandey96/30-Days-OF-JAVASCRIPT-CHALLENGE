7e4c1ce81abe806cddefb3b7a1f67d2306614eac
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 12:02:38 +0530

    Create script.js

---
+++ 09:Day9/Dom-Manipulation/activity5-event-handling/task10/script.js
@@ -0,0 +1,4 @@
+// Task 10: Add a mouseover event listener to an element that changes its border color. 
+document.querySelector('h1').addEventListener('mouseover',function(){
+    this.style.borderColor='blue'
+})
