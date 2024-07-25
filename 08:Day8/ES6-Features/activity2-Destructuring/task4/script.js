09228edf28567090b69f6f3323657548f2aad045
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 10:40:52 +0530

    Create script.js

---
+++ 08:Day8/ES6-Features/activity2-Destructuring/task4/script.js
@@ -0,0 +1,9 @@
+// Task 4: Use obejct destructuring to extract  title and author from a book object and log them to the console.
+const book={
+    title:'java',
+    author:'dino',
+    year:2020
+}
+const {title,author}=book
+console.log(title);  //java
+console.log(author); // dino
