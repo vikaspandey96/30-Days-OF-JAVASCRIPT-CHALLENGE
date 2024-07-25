86868bad178c7c796ad96cea7d6eac6543984619
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-25 11:53:10 +0530

    Update script.js

--- 09:Day9/Dom-Manipulation/activity4-modifying-attributes-classes/task8/script.js
+++ 09:Day9/Dom-Manipulation/activity4-modifying-attributes-classes/task8/script.js
@@ -1,4 +1,9 @@
 // Task 8: Add and remove a CSS class to/from an HTML element. 
 const headings=document.querySelector('.heading')
 headings.classList.add('change-color')
+// console.log(headings);
+
+
+//Remove class change-color
+headings.classList.remove('change-color')
 console.log(headings);
