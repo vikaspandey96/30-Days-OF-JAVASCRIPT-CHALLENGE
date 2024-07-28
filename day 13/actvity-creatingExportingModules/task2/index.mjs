6cb1ef9c0d797b220fb6991688bddc698ead2c07
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-28 11:35:07 +0530

    Update index.mjs

--- day 13/actvity-creatingExportingModules/task2/index.mjs
+++ day 13/actvity-creatingExportingModules/task2/index.mjs
@@ -1,5 +1,4 @@
-// Task 1: Create a module that exports a function to add two numbers. Imports and use this module in another script.
-import {person} from "./person.mjs";
+Task 2:Create a module that exports an object representing a person with properties and methods.Imports and use this module in another script. import {person} from "./person.mjs";
 console.log(`First name:${person.fname}`);
 console.log(`Last name :${person.lname}`);
 console.log(`Fullname name: ${person.fullName()}`);
