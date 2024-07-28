5a58f2969d63548d2238f62b011489e9c1ef6d87
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-28 11:16:54 +0530

    Create index.mjs

---
+++ day 13/actvity-creatingExportingModules/task1/index.mjs
@@ -0,0 +1,9 @@
+// Task 1: Create a module that exports a function to add two numbers. Imports and use this module in another script.
+import {addNumber} from "./math.mjs";
+const num1=44
+const num2=23
+const result=addNumber(num1,num2)
+console.log(result);
+
+
+//output: 67
