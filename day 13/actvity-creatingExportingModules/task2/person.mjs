0a33d6639857b71e5a8174547f6a297b2321bdaa
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-28 11:34:24 +0530

    Create person.mjs

---
+++ day 13/actvity-creatingExportingModules/task2/person.mjs
@@ -0,0 +1,5 @@
+Task 2:Create a module that exports an object representing a person with properties and methods.Imports and use this module in another script. 
+import {person} from "./person.mjs";
+console.log(`First name:${person.fname}`);
+console.log(`Last name :${person.lname}`);
+console.log(`Fullname name: ${person.fullName()}`);
