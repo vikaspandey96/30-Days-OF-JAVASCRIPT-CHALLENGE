129ab6ef8f51f04639573333ca4f607ee3a51302
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-07-28 12:08:14 +0530

    Create math.mjs

---
+++ day 13/activity2-namedAndDefaultExports/task4/math.mjs
@@ -0,0 +1,7 @@
+// Task 4: Create a module that exports a single function using defalult export.Import and use this function in another script. 
+ function addNumber(a, b) {
+    return a + b;
+  }
+  
+export default addNumber;
+  
