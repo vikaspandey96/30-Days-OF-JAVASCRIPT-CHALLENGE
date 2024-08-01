afdf1aa9e8a34c9f873f0b1ebe181b1e0ce144ce
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-08-01 16:16:13 +0530

    Update script.js

--- day16/Recursion/script.js
+++ day16/Recursion/script.js
@@ -6,3 +6,16 @@ function facto(num){
     return num*facto(num-1)
 }
 console.log(facto(5));
+
+
+// OR 
+function facto(num){
+    if(num===0){
+        return 1;
+    }
+    return num*facto(num-1)
+}
+console.log(facto(5));
+
+
+
