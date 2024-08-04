d52902f705305bccf78f97e5f2f5598b3ea6a73c
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-08-04 11:27:12 +0530

    Create script.js

---
+++ day17/linkedList/task1/script.js
@@ -0,0 +1,15 @@
+class node{
+    constructor(value){
+        this.value=value
+        this.next=null
+    }
+}
+const node1=new node(22)
+const node2=new node(10)
+node1.next=node2
+console.log(node1);
+console.log(node2);
+
+// output:/* 
+node { value: 22, next: node { value: 10, next: null } }
+node { value: 10, next: null } */
