0a27b2585d2d0d47fafc40e5f69cd3dea2cf3d99
vikas pandey <66833955+vikaspandey96@users.noreply.github.com>
2024-08-06 18:12:24 +0530

    Update person.mjs

--- day 13/actvity-creatingExportingModules/task2/person.mjs
+++ day 13/actvity-creatingExportingModules/task2/person.mjs
@@ -1,5 +1,9 @@
-Task 2:Create a module that exports an object representing a person with properties and methods.Imports and use this module in another script. 
-import {person} from "./person.mjs";
-console.log(`First name:${person.fname}`);
-console.log(`Last name :${person.lname}`);
-console.log(`Fullname name: ${person.fullName()}`);
+// Task 2:Create a module that exports an object representing a person with properties and methods.Imports and use this module in another script.  
+export {person}
+const person={
+    fname:"vikas",
+    lname:"pandey",
+    fullname(){
+        return this.fname +"" + this.lname
+    }
+}
